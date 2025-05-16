137ba5c77408f5ef3b54c27502bc3cab33121176
Priyam Dutta <priyamdutta624@gmail.com>
2025-05-16 10:26:26 +0530

    first commit

---
+++ Step 1/Lec 4/countDigits.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int countDigits(int num) {
+
+}
+
+int main() {
+    int num;
+    cout << "Enter the number: ";
+    cin >> num;
+
+    int digitNumber = countDigits(num);
+    cout << "Number of digits are: " << digitNumber;
+
+    return 0;
+}
