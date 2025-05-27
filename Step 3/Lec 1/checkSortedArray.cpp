fa7d0bd2efb8939c6ff53d283823a44dfe82e37f
Priyam Dutta <priyamdutta624@gmail.com>
2025-05-27 07:31:30 +0530

    format code

--- Step 3/Lec 1/checkSortedArray.cpp
+++ Step 3/Lec 1/checkSortedArray.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 
 bool checkArray(vector<int> nums) {
-        int checkBreak = 0;
+    int checkBreak = 0;
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] > nums[(i + 1) % nums.size()]) {
-                checkBreak++;
-            }
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] > nums[(i + 1) % nums.size()]) {
+            checkBreak++;
         }
-
-        return checkBreak <= 1 ? true : false;
     }
+
+    return checkBreak <= 1 ? true : false;
+}
 
 int main() {
     int size;
